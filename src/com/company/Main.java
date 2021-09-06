e2e6ff7cd487f31370fa0d91f7bb0ffa1347a96f
Timhansen1990 <timx1776@edu.easj.dk>
2021-09-06 09:08:40 +0200

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World ! ");
+        String day = "Sunday";
+        String month = "September 5";
+        int year = 2021;
+            System.out.print("American Format:");
+            System.out.print(day);
+            System.out.print(", ");
+            System.out.print(month);
+            System.out.print(", ");
+            System.out.print(year);
     }
 }
