5021628496dd77f1ca6935b1177c185d3938d4b1
tim Hansen <timx1776@edu.easj.dk>
2021-09-06 15:40:02 +0200

    2 tabellen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-
+        System.out.println("Her er 2 tabellen:");
         for (int i = 2; i <= 20; i = i + 2) {
             System.out.println(i);
-//            her laves 2 tabellen //
+// her laves 2 tabellen //
 
-            }
         }
     }
+}
 
 
